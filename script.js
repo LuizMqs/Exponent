df16b27768532f9675bfcf42a36c87960764a0e6
Luiz Antônio Marques Junior <103662405+LuizMqs@users.noreply.github.com>
2022-06-16 16:45:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+function sum(num1, num2) {
+  if (
+    num1 == parseInt(num1) &&
+    num2 == parseInt(num2) &&
+    num1 >= 0 &&
+    num2 >= 0
+  ) {
+    result = num1 + num2;
+    return result;
+  } else {
+    throw new Error(`[multiply] Impossible to multiply ${num1} * ${num2}`);
+  }
+}
+
+function mult(num1, num2, base, result) { 
+
+  if (num1 < 0 || num2 < 0) {
+    throw `[multiply] impossible to multiply ${num1} * ${num2}`;
+  }
+
+  if (base >= num2) {
+    return result;
+  }
+
+  return mult(num1, num2, sum(base, 1), sum(result, num1));
+}
+
+function expo(num1, num2, base, result) {
+
+  if (base >= num2) {
+    return result;
+  }
+
+  return expo(num1, num2, sum(base, 1), mult(result, num1, 0, 0));
+}
+
+document.getElementById("button").addEventListener("click", function () {
+  document.getElementById("result").innerHTML = "";
+
+  const num1 = Number(document.getElementById("1").value);
+  const num2 = Number(document.getElementById("2").value);
+  let result = expo(num1, num2, 0, 1);
+
+  document.getElementById("result").innerHTML = result;
+});
